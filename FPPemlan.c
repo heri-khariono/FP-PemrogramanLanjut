bea1f12065616409f1b58be4a7df461f4476eb7c
Heri Khariono <54542558+heri-khariono@users.noreply.github.com>
2019-12-16 20:08:19 +0700

    Penambahan struct

--- FPPemlan.c
+++ FPPemlan.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <math.h>
 
-int main() {
-	
-	return 0;
+struct pelabuhan{
+	int no_tiket;
+	int tujuan;
+	int d;
+	int m;
+	int y;
+	char nopol[60];
+	int jenis_kendaraan;
+	int bobot;
+	char kjenis_kendaraan[60];
+	char ktujuan[60];
+	char kbulan[60];
+}data[60];
+
+int i=0;
+
+int main(){
+
+return 0;
 }
