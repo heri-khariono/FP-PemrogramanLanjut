f0b80a74238186077744459f425d4863686455c4
fans-lab <54657807+fans-lab@users.noreply.github.com>
2019-12-17 23:47:48 +0700

    tambahan menu program

--- FPPemlan.c
+++ FPPemlan.c
@@ -20,7 +20,56 @@ struct pelabuhan{
 
 int i=0;
 
-int main(){
-
-return 0;
+int main() {
+	int menu;
+	system("color f0");
+	
+	utama :
+		printf("+===========================================================+\n");
+		printf("|             FINAL PROJECT PEMROGRAMAN LANJUT              |\n");
+		printf("|___________________________________________________________|\n");
+		printf("|                   APLIKASI PELABUHAN                      |\n");
+		printf("|               180181010002 & 1534010107                   |\n");
+		printf("+===========================================================+\n\n");
+		printf("PILIHAN MENU : \n");
+		printf("1. Tambah Data\n");
+		printf("2. Tampilkan Data\n");
+		printf("3. Ubah Data\n");
+		printf("4. Pencarian Data\n");
+		printf("5. Pengurutan Data\n\n");
+		printf("Masukkan Pilihan Menu : ");
+		scanf("%d", &menu);
+		
+		switch (menu){
+			case 1 : 
+				system("cls");
+				goto utama;
+				break;
+			case 2 :
+				system("cls");
+				getch();
+				system("cls");
+				goto utama;
+				break;
+			case 3 :
+				system("cls");
+				goto utama;
+				break;
+			case 4 :
+				system("cls");
+				goto utama;
+				break;
+			case 5 :
+				system("cls");
+				goto utama;
+				break;
+			default :
+				system("cls");
+				printf("\n\t\tPilihan menu tidak tersedia !!\n");
+				getch();
+				system("cls");
+				goto utama;
+				break;
+	}	
+	return 0;
 }
